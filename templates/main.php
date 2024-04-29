0434c9aeb9d533e6f912da0fb989c7188f890b0a
Dante <danterossi99@gmail.com>
2024-04-29 11:46:17 -0300

    no-pre

--- templates/main.php
+++ templates/main.php
@@ -1,7 +1,7 @@
 <main>
-    <pre style="font-size: 15px; overflow: scroll; height: 400px;">
-        <?php var_dump($data); ?>
-    </pre>
+<!--     <pre style="font-size: 15px; overflow: scroll; height: 400px;">
+        <##?php var_dump($data); ?>
+    </pre> -->
     <section>
         <h1>Planetas activos: <?= count($data);?> </h1>
 
